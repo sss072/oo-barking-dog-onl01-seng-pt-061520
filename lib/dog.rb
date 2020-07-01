90d9cf124ac9a2f5d7544c9edac1deb1886893d2
Sami  Sabeeh <samsabeeh6@gmail.com>
2020-07-01 23:11:23 +0000

    Done.

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,1 +1,11 @@
-# Your code goes here!
+class Dog 
+  def name=(dogs_name)
+    @the_consistent_name= dogs_name
+  end
+  def name 
+    @the_consistent_name
+  end
+  def bark 
+    puts "woof!"
+  end
+end
